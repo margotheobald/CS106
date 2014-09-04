7485503c4900ff1e96e4f69018960e4d70f9172c
margotheobald <funnymardoo@gmail.com>
2014-09-03 20:18:48 -0400

    Adding Hello World.

---
+++ Hello World/src/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Margo
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
